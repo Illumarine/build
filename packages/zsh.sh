16fd94a14fcbc8bb23e2467547f917d2cb661cb3
BBBensonDev <barry.b.benson.dev@gmail.com>
2025-07-10 16:22:51 -0600

    Add ZSH package to test with

---
+++ packages/zsh.sh
@@ -0,0 +1,19 @@
+PROTO_DIR=${HOME}/illumos-gate/proto/root_i386
+
+while getopts "o:p:b:" opt; do
+  case $opt in
+      # proto directory
+      PROTO_DIR="$OPTARG"
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+wget https://www.zsh.org/pub/zsh-5.9.tar.xz
+tar -xvf zsh-5.9.tar.xz
+cd zsh-5.9
+./configure --prefix=${PROTO_DIR}/usr/local
+make
+make install
+cd ..
+rm -rf zsh-5.9/
