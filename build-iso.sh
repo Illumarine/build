9de0517591a101ec7b26f7f05a3b95735d9b5a3a
BBBensonDev <barry.b.benson.dev@gmail.com>
2025-07-10 13:25:07 -0600

    Fix some things up

--- build-iso.sh
+++ build-iso.sh
@@ -1,19 +1,27 @@
 MVI_DIR=${HOME}/build
 PROTO_DIR=${HOME}/illumos-gate/proto/root_i386
+ISO_NAME=${HOME}/build-$$.iso
 
-while getopts "m:p:" opt; do
+while getopts "o:p:b:" opt; do
   case $opt in
-    m)
-      MVI_DIR="$OPTARG"
+    o)
+      # output file
+      ISO_NAME="$OPTARG"
       ;;
     p)
+      # proto directory
       PROTO_DIR="$OPTARG"
+      ;;
+    b)
+      # MVI or build directory
+      MVI_DIR="$OPTARG"
       ;;
     *)
 	    bail "invalid argument $opt"
 	    ;;
   esac
 done
+shift $((OPTIND-1))
 
 if [ ! -f ${MVI_DIR}/iso/pureboot.sh ]; then
   echo "Pureboot script not found!"
@@ -30,4 +38,4 @@ if [ ! -f ${MVI_DIR}/iso/uefi.sh ]; then
   exit 1
 fi
 
-sh ${MVI_DIR}/iso/pureboot.sh -o /tmp/build_$$.iso -p ${PROTO_DIR} -b ${MVI_DIR}
+sh ${MVI_DIR}/iso/pureboot.sh -p ${PROTO_DIR} -b ${MVI_DIR} -o ${ISO_NAME}
--- build.sh
+++ build.sh
@@ -1,19 +1,27 @@
 MVI_DIR=${HOME}/build
 PROTO_DIR=${HOME}/illumos-gate/proto/root_i386
+ISO_NAME=${HOME}/build-$$.iso
 
-while getopts "m:p:" opt; do
+while getopts "o:p:b:" opt; do
   case $opt in
-    m)
-      MVI_DIR="$OPTARG"
+    o)
+      # output file
+      ISO_NAME="$OPTARG"
       ;;
     p)
+      # proto directory
       PROTO_DIR="$OPTARG"
+      ;;
+    b)
+      # MVI or build directory
+      MVI_DIR="$OPTARG"
       ;;
     *)
 	    bail "invalid argument $opt"
 	    ;;
   esac
 done
+shift $((OPTIND-1))
 
 # TODO: Build packages in proto before building ISO
-sh ${MVI_DIR}/build-iso.sh -m ${MVI_DIR} -p ${PROTO_DIR}
+sh ${MVI_DIR}/build-iso.sh -m ${MVI_DIR} -p ${PROTO_DIR} -o ${ISO_NAME}
