346316bc684954556ee9d6b9a5ed1a7e5e79c029
BBBensonDev <barry.b.benson.dev@gmail.com>
2025-07-15 20:50:01 -0600

    Make sure root is mounted

--- iso/pureboot.sh
+++ iso/pureboot.sh
@@ -137,8 +137,8 @@ EOF
 cd ${DESTDIR} || bail "cannot cd to ${DESTDIR}"
 cat > ${DESTDIR}/etc/mvi.rc <<EOF
 #!/sbin/sh
+mount -F tmpfs tmpfs /root
 echo " *** Welcome to Illumarine ***" > /dev/console
-cd /root
 /bin/ksh93 >/dev/console 2>&1 </dev/console
 EOF
 chmod a+x ${DESTDIR}/etc/mvi.rc
